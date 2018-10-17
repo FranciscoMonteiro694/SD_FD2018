7596d407c3b1200019f9618fb3295de24fd04fca
FranciscoMonteiro694 <34212758+FranciscoMonteiro694@users.noreply.github.com>
2018-10-17 18:30:19 +0100

    Update MulticastServer.java

--- code/MulticastServer.java
+++ code/MulticastServer.java
@@ -1,23 +1,17 @@
-import java.util.HashMap;
 import java.net.MulticastSocket;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.io.IOException;
-
-
-class Mensagem{
-    private HashMap<String, String> map;
-
-    Mensagem(HashMap<String, String> map) {
-        this.map = map;
-    }
-
-}
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+import java.io.BufferedInputStream;
+import java.util.HashMap;
 
 public class MulticastServer extends Thread {
     private String MULTICAST_ADDRESS = "224.0.224.0";
-    private int PORT = 4321;
-    private long SLEEP_TIME = 5000;
+    private int PORT_ENV = 4321; // Porto para enviar
+    private int PORT_REC = 4322; // Porto para receber
+
 
     public static void main(String[] args) {
         MulticastServer server = new MulticastServer();
@@ -25,28 +19,25 @@ public class MulticastServer extends Thread {
     }
 
     public MulticastServer() {
-        super("Server " + (long) (Math.random() * 1000));
+        super("Server a correr " + (long) (Math.random() * 1000));
     }
-
 
     public void run() {
         MulticastSocket socket = null;
-        long counter = 0;
-        System.out.println(this.getName() + " running...");
+        System.out.println(this.getName());
         try {
-            socket = new MulticastSocket();  // create socket without binding it (only for sending)
-            while (true) {
-                String message = this.getName() + " packet " + counter++;
-                byte[] buffer = message.getBytes();
-
-                InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, PORT);
-                socket.send(packet);
-
-                try {
-                    sleep((long) (Math.random() * SLEEP_TIME));
-                } catch (InterruptedException e) { }
+            socket = new MulticastSocket(PORT_ENV);
+            InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
+            socket.joinGroup(group);
+            while(true) {
+                byte[] buffer = new byte[256];
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+                System.out.println("Received packet from " + packet.getAddress().getHostAddress() + ":" + packet.getPort() + " with message:");
+                String message = new String(packet.getData(), 0, packet.getLength());
+                System.out.println(message);
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -54,3 +45,5 @@ public class MulticastServer extends Thread {
         }
     }
 }
+
+
