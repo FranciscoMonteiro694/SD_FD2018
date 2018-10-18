f214a4d31d75a8ce7f21c087181c775eea31b757
FranciscoMonteiro694 <34212758+FranciscoMonteiro694@users.noreply.github.com>
2018-10-19 00:22:29 +0100

    Add files via upload

--- code/MulticastServer.java
+++ code/MulticastServer.java
@@ -2,31 +2,34 @@ import java.net.MulticastSocket;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.io.IOException;
-import java.io.ByteArrayInputStream;
-import java.io.ObjectInputStream;
-import java.io.BufferedInputStream;
-import java.util.HashMap;
+import java.lang.*;
 
 public class MulticastServer extends Thread {
+    private static int server_id;
     private String MULTICAST_ADDRESS = "224.0.224.0";
-    private int PORT_ENV = 4321; // Porto para enviar
-    private int PORT_REC = 4322; // Porto para receber
+    private int PORT_REC = 4321; // Porto para receber
+    private int PORT_ENV = 4322; // Porto para enviar
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args) { //meter um nº de servidor
+        String teste = args[0];
+        server_id = Integer.parseInt(teste);
         MulticastServer server = new MulticastServer();
         server.start();
+        
+        System.out.println("Servidor com nº : "+ server_id);
+
     }
 
     public MulticastServer() {
-        super("Server a correr " + (long) (Math.random() * 1000));
+        super("Server running " + server_id);
     }
 
     public void run() {
         MulticastSocket socket = null;
         System.out.println(this.getName());
+
         try {
-            socket = new MulticastSocket(PORT_ENV);
+            socket = new MulticastSocket(PORT_REC);
             InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
             socket.joinGroup(group);
             while(true) {
